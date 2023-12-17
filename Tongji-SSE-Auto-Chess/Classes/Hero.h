dbbab34c1db0b2a004205cef12f133051b283f89
L1KEAB0T <wjy1370858716@gmail.com>
2023-12-17 17:02:01 +0800

    加入英雄池头文件

---
+++ Tongji-SSE-Auto-Chess/Classes/Hero.h
@@ -0,0 +1,17 @@
+#pragma once
+#include"HelloWorldScene.h"
+#include "cocos2d.h"
+#include <goldenshovel_hero_design.h>
+using namespace std;
+
+
+//°²ÄÝ https://lol.qq.com/tft/#/championDetail/1
+MyHero Annie(0, 1, 1, 40, 70, 7, 0, 4, 0.7, 500, -1, -1);
+//¿âÆæ https://lol.qq.com/tft/#/championDetail/42
+MyHero Corki(0, 1, 1, 48, 80, 8, 0, 4, 0.7, 450, -1, -1);
+//ËþÀï¿Ë https://lol.qq.com/tft/#/championDetail/44
+MyHero Taric(0, 1, 1, 55, 60, 12, 0, 1, 0.55, 650, -1, -1);
+//ÒÁÜ½ÁÕ https://lol.qq.com/tft/#/championDetail/12
+MyHero Evelynn(0, 1, 1, 45, 60, 12, 0, 1, 0.7, 550, -1, -1);
+MyHero one_fee[4] = {Annie,Corki,Taric,Evelynn};
+
