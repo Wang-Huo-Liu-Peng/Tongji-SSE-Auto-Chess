9407264b37216f3fdc1ae4d695038f32a517d436
CoArrayLiu <3061632379@qq.com>
2023-12-17 16:18:51 +0800

    英雄类的大招与攻击函数

--- Tongji-SSE-Auto-Chess/Classes/goldenshovel_hero_design.h
+++ Tongji-SSE-Auto-Chess/Classes/goldenshovel_hero_design.h
@@ -4,6 +4,9 @@
 using namespace std;
 #define plaid_width 256
 #define plaid_height 200
+
+#define attack_ace 1    //攻击性大招
+#define treat_ace 2     //治疗型大招
 /*====================基类====================*/
 //注意：该基类为抽象基类，不可直接生成对象
 class MyObject {
@@ -47,8 +50,9 @@ public:
 		battle_index(index), gold_cost(cost), star_level(level), attack_power(power), ace_attack_power(a_power),
 		needed_cooldown_round(n_c_r), current_cooldown_round(c_c_r) {};
 	//virtual void my_move(int new_x, int new_y);  // 移动函数
-    void seek_enemy(MyHero hero, vector<MyHero>& Hero_on_court);                           // 索敌函数
-	void hero_ultimate();                        // 大招函数
+    void seek_enemy(MyHero hero);                           // 索敌函数
+    void hero_attack();                           //攻击函数
+	void hero_ultimate(int ace_mode);                        // 大招函数
 
 	MyHero* current_enemy;
 private:
@@ -59,12 +63,12 @@ private:
 	int ace_attack_power;              // 大招攻击力
 	int needed_cooldown_round;         // 大招所需冷却轮数
     int current_cooldown_round;        // 当前冷却轮数
-    int attack_distance;
-    // 攻击距离
+    int attack_distance;               // 攻击距离
+    int attack_cd;                     //攻击速度
 };
 
 //英雄索敌
-void MyHero::seek_enemy(MyHero hero, vector <MyHero>& Hero_on_court) {
+void MyHero::seek_enemy(MyHero hero) {
     BOOL find = 0;
     int distance = 1;
     int i;
@@ -91,6 +95,62 @@ void MyHero::seek_enemy(MyHero hero, vector <MyHero>& Hero_on_court) {
             hero.location_x = enemy.location_x + hero.attack_distance * plaid_width * (hero.location_x - enemy.location_x) / abs(hero.location_x - enemy.location_x);
             hero.location_y = enemy.location_y + hero.attack_distance * plaid_height * (hero.location_y - enemy.location_y) / abs(hero.location_y - enemy.location_y);
             //hero.sprite 设计moveby和moveto，还没写
+        }
+    }
+}
+
+void MyHero::hero_attack() {
+    if (current_enemy == NULL) {   //没有攻击目标或者攻击目标死亡后未重新寻找目标
+        seek_enemy(*this);
+    }
+    int last_attack_time;
+    while (current_enemy != NULL) {
+        last_attack_time = time(NULL);              //记录攻击的时间点
+        //攻击动画
+        if (this->attack_power < current_enemy->current_hp) {  //不能直接击杀
+            current_enemy->current_hp -= this->attack_power;
+            //current_enemy->sprite->minus_hp(); //掉血动画
+        }
+        else {                                                 //直接击杀
+            current_enemy->current_hp = 0;
+            //current_enemy->sprite->minus_hp(); //掉血动画
+            current_enemy = NULL;
+        }
+        if (current_enemy == NULL)            //击杀后将敌人置为NULL退出循环
+            break;
+        while (1) {                             //攻击cd
+            int now_time = time(NULL);
+            if (now_time - last_attack_time >= attack_cd)
+                break;
+        }
+    }
+}
+
+void MyHero::hero_ultimate(int ace_mode)                        // 大招函数
+{
+    if (current_enemy == NULL) {
+        seek_enemy(*this);  //调用索敌函数
+    }
+    //this->Sprite->perform_ace();//释放大招的动画，如果有
+    if (ace_mode == attack_ace)
+    {
+        if (!(ace_attack_power < current_enemy->current_hp)) {  //直接击杀
+            current_enemy->current_hp = 0;
+            //attacking_hero->sprite->minus_hp_animation();//掉血条动画
+        }
+        else {      //未直接击杀
+            current_enemy->current_hp -= ace_attack_power;
+            //attacking_hero->sprite->minus_hp_animation();//掉血条动画
+        }
+    }
+    else if (ace_mode == treat_ace) {
+        if (ace_attack_power < (current_enemy->full_hp - current_hp)) {     //回满血
+            current_enemy->current_hp += ace_attack_power;
+            //attacking_hero->sprite->add_hp_animation();//回血条动画
+        }
+        else {      //不够回满血
+            current_enemy->current_hp = full_hp;
+            //attacking_hero->sprite->add_hp_animation();//回血条动画
         }
     }
 }
