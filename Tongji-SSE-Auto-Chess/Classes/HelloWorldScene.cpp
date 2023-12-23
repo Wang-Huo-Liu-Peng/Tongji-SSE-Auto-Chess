6d41bfaf61c7e42d787bca97920499ccb8fd257a
Huo Yicheng <huohuoyicheng@gmail.com>
2023-12-23 17:54:34 +0800

    网络连接测试

--- Tongji-SSE-Auto-Chess/Classes/HelloWorldScene.cpp
+++ Tongji-SSE-Auto-Chess/Classes/HelloWorldScene.cpp
@@ -2,39 +2,9 @@
 #include "SimpleAudioEngine.h"
 #include "Test.h"
 #include <Show_Chinese.h>
-#include "network/HttpClient.h"
+#include "NetworkConnection.h"
 
 USING_NS_CC;
-using namespace network;
-
-class NetworkManager {
-public:
-    static NetworkManager* getInstance() {
-        static NetworkManager instance;
-        return &instance;
-    }
-
-    void checkNetworkConnection() {
-        HttpRequest* request = new HttpRequest();
-        request->setUrl("http://www.google.com");
-        request->setRequestType(HttpRequest::Type::GET);
-        request->setResponseCallback(CC_CALLBACK_2(NetworkManager::onNetworkCheckResponse, this));
-        HttpClient::getInstance()->send(request);
-        request->release();
-    }
-
-private:
-    void onNetworkCheckResponse(HttpClient* client, HttpResponse* response) {
-        if (response && response->isSucceed()) {
-            CCLOG("Network is connected");
-            // 在这里执行连接成功后的逻辑
-        }
-        else {
-            CCLOG("Network is not connected");
-            // 在这里执行连接失败后的逻辑
-        }
-    }
-};
 
 Scene* HelloWorld::createScene()
 {
--- Tongji-SSE-Auto-Chess/Classes/NetworkConnection.h
+++ Tongji-SSE-Auto-Chess/Classes/NetworkConnection.h
@@ -1,66 +1,33 @@
-#pragma once
-//#include "cocos2d.h"
-//#include "network/HttpClient.h"
-//
-//USING_NS_CC;
-//using namespace network;
-//
-//class NetworkManager {
-//public:
-//    static NetworkManager* getInstance() {
-//        static NetworkManager instance;
-//        return &instance;
-//    }
-//
-//    void checkNetworkConnection() {
-//        HttpRequest* request = new HttpRequest();
-//        request->setUrl("http://www.google.com");
-//        request->setRequestType(HttpRequest::Type::GET);
-//        request->setResponseCallback(CC_CALLBACK_2(NetworkManager::onNetworkCheckResponse, this));
-//        HttpClient::getInstance()->send(request);
-//        request->release();
-//    }
-//
-//private:
-//    void onNetworkCheckResponse(HttpClient* client, HttpResponse* response) {
-//        if (response && response->isSucceed()) {
-//            CCLOG("Network is connected");
-//            // 在这里执行连接成功后的逻辑
-//        }
-//        else {
-//            CCLOG("Network is not connected");
-//            // 在这里执行连接失败后的逻辑
-//        }
-//    }
-//};
-//
-//class HelloWorld : public cocos2d::Scene {
-//public:
-//    virtual bool init() {
-//        if (!Scene::init()) {
-//            return false;
-//        }
-//
-//        // 使用 NetworkManager 检查网络连接
-//        NetworkManager::getInstance()->checkNetworkConnection();
-//
-//        return true;
-//    }
-//
-//    CREATE_FUNC(HelloWorld);
-//};
-//
-//int main(int argc, char** argv) {
-//    auto director = cocos2d::Director::getInstance();
-//    auto glview = director->getOpenGLView();
-//
-//    if (!glview) {
-//        glview = cocos2d::GLViewImpl::create("My Cocos Game");
-//        director->setOpenGLView(glview);
-//    }
-//
-//    auto scene = HelloWorld::create();
-//    director->runWithScene(scene);
-//
-//    return 0;
-//}
+#include "network/HttpClient.h"
+
+USING_NS_CC;
+using namespace network;
+
+class NetworkManager {
+public:
+    static NetworkManager* getInstance() {
+        static NetworkManager instance;
+        return &instance;
+    }
+
+    void checkNetworkConnection() {
+        HttpRequest* request = new HttpRequest();
+        request->setUrl("http://www.google.com");
+        request->setRequestType(HttpRequest::Type::GET);
+        request->setResponseCallback(CC_CALLBACK_2(NetworkManager::onNetworkCheckResponse, this));
+        HttpClient::getInstance()->send(request);
+        request->release();
+    }
+
+private:
+    void onNetworkCheckResponse(HttpClient* client, HttpResponse* response) {
+        if (response && response->isSucceed()) {
+            CCLOG("Network is connected");
+            // 在这里执行连接成功后的逻辑
+        }
+        else {
+            CCLOG("Network is not connected");
+            // 在这里执行连接失败后的逻辑
+        }
+    }
+};
