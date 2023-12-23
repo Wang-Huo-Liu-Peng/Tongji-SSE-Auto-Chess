3f2fb7b385ed768971088f200bbef8cc51d61202
Rock1ey <2061218098@qq.com>
2023-12-23 14:37:20 +0800

    小修改

--- Tongji-SSE-Auto-Chess/Classes/BattleLayer.h
+++ Tongji-SSE-Auto-Chess/Classes/BattleLayer.h
@@ -11,44 +11,33 @@ class BattleLayer : public cocos2d::Layer
 public:
     virtual bool init();
 
-    MySprite* player_red;         //红方玩家
-    MySprite* player_blue;        //蓝方玩家
+    MySprite redPlayer;         //红方玩家
+    MySprite bluePlayer;        //蓝方玩家
+    vector<MyHero> blueHero;     //蓝方英雄（场上）
+    vector<MyHero> redHero;      //红方英雄（场上）
+
+    void allocate(MySprite&,MySprite&);
+    void addHero(vector<MyHero>& Hero_fighting);
+
     vector<Bullet> bullet;        //管理子弹
 
-    //void schedule(SEL_SCHEDULE selector); //定时器,逐帧刷新
-    //void schedule(SEL_SCHEDULE selector,float interval); //定时器，隔interval秒刷新
-    void myupdate(float dt);        //游戏中需要时刻更新的东西
+    void myupdate(float dt);      //游戏中需要时刻更新的东西
     void update_attack(float dt); //平A更新
 
-    bool IsZeroHp(MyHero& hero);  //英雄是否死亡
-    void check_death(vector<MyHero>& my_vec, vector<MyHero>& enemy_vec);//移出死亡英雄
-    void BulletMove();            //子弹移动
-    void BulletDelete();          //子弹回收，英雄扣血
-    void HeroMove(vector<MyHero>&);//英雄移动
-
+    void BattleLayer::check_death(vector<MyHero>& Hero_fighting);//移出死亡英雄
+    void checkBullet();           //子弹回收，英雄扣血
+    void checkUltimate(vector<MyHero>& Hero_fighting);//蓝条满放大招
+    void seekAndMove(vector<MyHero>&);//英雄索敌并移动
     MyHero hero1;
     MyHero hero2;
-    /*
-    void Delete_death(vector<MyHero>& Hero_fighting) {
-        auto it = Hero_fighting.begin();
-        while (it != Hero_fighting.end()) {
-            if (it->gethp() == 0) {
-                this->removeChild(it->sprite, true);
-                it = Hero_fighting.erase(it);
-            }
-            else {
-                ++it;
-            }
-        }
-    }*/
+    MyHero hero3;
+
     //virtual void onEnter();
     // a selector callback
     //void menuCloseCallback(cocos2d::Ref* pSender);
 
     // implement the "static create()" method manually
     CREATE_FUNC(BattleLayer);
-
-    //void battle();
 };
 
 #endif // __BATTLE_LAYER_H__  
