e916eae1b0b425293e82bd47ae0b48d547ad4615
Rock1ey <2061218098@qq.com>
2023-12-28 21:17:05 +0800

    报错的地方注释了

--- Tongji-SSE-Auto-Chess/Classes/passinfo.h
+++ Tongji-SSE-Auto-Chess/Classes/passinfo.h
@@ -2,8 +2,7 @@
 #ifndef __PASSINFO__H__
 #define __PASSINFO__H__
 
-#include<string>
-
+//#include "goldenshovel_hero_design.h"
 
 #define max_command_length 32
 #define max_ac_num  12
@@ -20,12 +19,16 @@
 #define Login "LOGIN"
 #define Exit  "Exit"
 
+using namespace std;
 
 struct passinfo {
 	char event[max_command_length] = { '0' };
 
 	char account_number[max_ac_num] = { '0' };
 	char password[max_ac_pw] = { '0' };
+
+	//vector<MyHero> hero_on_court;
+	//vector<MyHero> hero_fighting;
 
 	int _result;
 };
