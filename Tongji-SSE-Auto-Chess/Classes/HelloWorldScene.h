54fba22339b862bf7af1281b74b457975b87eb48
L1KEAB0T <wjy1370858716@gmail.com>
2023-12-31 20:21:41 +0800

    加入线程池防止没联网时进入卡顿

--- Tongji-SSE-Auto-Chess/Classes/HelloWorldScene.h
+++ Tongji-SSE-Auto-Chess/Classes/HelloWorldScene.h
@@ -6,12 +6,15 @@
 class HelloWorld : public cocos2d::Scene
 {
 public:
-    static cocos2d::Scene* createScene();
-
+    static cocos2d::Scene* createScene(); 
+    std::shared_future<bool> connectionFuture; // 异步任务的 shared_future 对象
+    std::promise<bool> connectionPromise; // 异步任务的 promise 对象
     virtual bool init();
-    
+
     // a selector callback
     void menuCloseCallback(cocos2d::Ref* pSender);
+
+    void update(float delta);
     
     // implement the "static create()" method manually
     CREATE_FUNC(HelloWorld);
